b7b302b272f5473f64e3ab414599692051bb733c
AmpTheDev <gerritegberink3@gmail.com>
2020-04-29 23:03:37 +0200

    Set up workspace using PufferfishGradle

---
+++ src/mc1.15.2/java/dev/cbyrne/pufferfishmodloader/launch/PMLClientMain.java
@@ -0,0 +1,9 @@
+package dev.cbyrne.pufferfishmodloader.launch;
+
+import net.minecraft.client.main.Main;
+
+public class PMLClientMain {
+    public static void main(String... args) {
+        Main.main(args);
+    }
+}
