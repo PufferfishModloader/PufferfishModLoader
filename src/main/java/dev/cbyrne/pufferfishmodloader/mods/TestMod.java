35ceafade0ce0d9c6c2626fab9ba9bf494d68219
Conor Byrne <con0r.b@protonmail.com>
2020-05-02 20:41:35 +0100

    Remove logging in TestMod

--- src/main/java/dev/cbyrne/pufferfishmodloader/mods/TestMod.java
+++ src/main/java/dev/cbyrne/pufferfishmodloader/mods/TestMod.java
@@ -4,7 +4,6 @@ import dev.cbyrne.pufferfishmodloader.events.EventBus;
 import dev.cbyrne.pufferfishmodloader.events.InvokeEvent;
 import dev.cbyrne.pufferfishmodloader.events.core.mod.ModInitEvent;
 import dev.cbyrne.pufferfishmodloader.mods.core.Mod;
-import org.apache.logging.log4j.LogManager;
 
 @Mod("bruh")
 public class TestMod {
@@ -14,6 +13,5 @@ public class TestMod {
 
     @InvokeEvent
     public void onInit(ModInitEvent event) {
-        LogManager.getLogger("TestMod").info("TestMod started!");
     }
 }
