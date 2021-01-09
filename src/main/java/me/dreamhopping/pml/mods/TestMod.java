c621acee0835006f7dae4dd4fd947b821c616455
dreamhopping <71222289+dreamhopping@users.noreply.github.com>
2021-01-09 23:56:21 +0000

    Implement listen for Chat Message in TestMod

--- src/main/java/me/dreamhopping/pml/mods/TestMod.java
+++ src/main/java/me/dreamhopping/pml/mods/TestMod.java
@@ -2,6 +2,7 @@ package me.dreamhopping.pml.mods;
 
 import me.dreamhopping.pml.events.EventBus;
 import me.dreamhopping.pml.events.InvokeEvent;
+import me.dreamhopping.pml.events.core.client.ChatMessageEvent;
 import me.dreamhopping.pml.events.core.mod.ModInitEvent;
 import me.dreamhopping.pml.mods.core.Mod;
 import org.apache.logging.log4j.LogManager;
@@ -18,5 +19,10 @@ public class TestMod {
     @InvokeEvent
     public void onInit(ModInitEvent event) {
         logger.info("Hello world! TestMod is now awake...");
+    }
+
+    @InvokeEvent
+    public void onChatMessage(ChatMessageEvent event) {
+        logger.info("TestMod received a chat message: " + event.message);
     }
 }
