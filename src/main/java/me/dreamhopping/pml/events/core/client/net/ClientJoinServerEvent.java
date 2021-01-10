9bef2d55f7a1c1befaacbc0837ee351d70194970
dreamhopping <71222289+dreamhopping@users.noreply.github.com>
2021-01-10 19:21:49 +0000

    Continue work on ClientJoinServerEvent

--- src/main/java/me/dreamhopping/pml/events/core/client/net/ClientJoinServerEvent.java
+++ src/main/java/me/dreamhopping/pml/events/core/client/net/ClientJoinServerEvent.java
@@ -2,10 +2,14 @@ package me.dreamhopping.pml.events.core.client.net;
 
 import me.dreamhopping.pml.events.Event;
 
+import java.net.SocketAddress;
+
 public class ClientJoinServerEvent extends Event {
     public final boolean isLocal;
+    public final SocketAddress address;
 
-    public ClientJoinServerEvent(boolean isLocal) {
+    public ClientJoinServerEvent(boolean isLocal, SocketAddress address) {
         this.isLocal = isLocal;
+        this.address = address;
     }
 }
